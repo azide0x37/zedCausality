db45ce90b32d4cd75469a664c344bc19b2e702c1
Alexander Templeton <blakmatr@gmail.com>
2017-05-19 23:45:43 -0500

    adding auth0 code, integrating...

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'ritual.auth0.com',
+  clientId: 'WxZ9C16LEJklJkMMeq7LP_2ANgvrJ9lq',
+  callbackUrl: 'http://localhost:3000/callback'
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { makeMainRoutes() } from './routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	routes, 
+	document.getElementById('root')
+);
 registerServiceWorker();
